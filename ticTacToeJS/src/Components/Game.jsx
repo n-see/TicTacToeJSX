f489e87474b3382609d327ce27c095127dcf1557
Neo See <neococo123@yahoo.com>
2024-06-20 21:05:48 -0700

    finished

--- ticTacToeJS/src/Components/Game.jsx
+++ ticTacToeJS/src/Components/Game.jsx
@@ -3,32 +3,36 @@ import Board from './Board'
 
 const Game = () => {
 
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[history.length - 1];
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
 
     const handlePlay = (nextSquares) => {
-        setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+        // setXIsNext(!xIsNext);
     }
 
- 
+
     const moves = history.map((squares, move) => {
         let description;
-        if (moves > 0){
-            description = 'Go to move # ' + move;
+        if (move > 0) {
+            description = 'Go to move #' + move;
         } else {
             description = 'Go to game start';
         }
         return (
-            <li>
+            <li key={move}>
                 <button onClick={() => jumpTo(move)}>{description}</button>
             </li>
-        )
-    })
+        );
+    });
 
-    function jumpTo(nextmove) {
-
+    const jumpTo = (nextMove) => {
+        setCurrentMove(nextMove);
+        // setXIsNext(nextMove % 2 === 0);
     }
 
 
@@ -36,7 +40,7 @@ const Game = () => {
         <>
             <div className="game">
                 <div className="game-board">
-                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+                    <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
                 </div>
                 <div className="game-info">
                     <ol>{moves}</ol>
